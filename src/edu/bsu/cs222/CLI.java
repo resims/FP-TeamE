b32e7a797f55d623aa5da5f447d40cf9963b2aae
Ryan Sims <resims@bsu.edu>
2018-10-08 02:51:05 -0400

    Implements sql integration in CLI.java We switch to using sql as out approach, as it takes care of the data structure and querying for us Additionally, it is more likely that a library will already be using an sql server

--- src/edu/bsu/cs222/CLI.java
+++ src/edu/bsu/cs222/CLI.java
@@ -1,14 +1,49 @@
 package edu.bsu.cs222;
 
 import java.util.Scanner;
+import java.sql.*;
+//import java.sql.
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class CLI {
+    static Connection conn = null;
     public static void main(String args[]) {
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost/library?" +
+                            "user=root&useSSL=false");
 
-    System.out.println("Library Management System 2.0");
-    Scanner scanner = new Scanner(System.in);
-    scanner.nextLine();
+
+            ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM books");
+            parse(rs);
+
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+    public static void parse(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        for (int i = 1; i <= metaData.getColumnCount(); i++) {System.out.print(metaData.getColumnName(i)+", ");}System.out.println();
+        while (resultSet.next()) {
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                if (i > 1) System.out.print(",  ");
+                String columnValue = resultSet.getString(i);
+                System.out.print(columnValue);
+            }
+            System.out.println();
+        }
+    }
 }
 
-}
+
+
+
+
+
