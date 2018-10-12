5619c9d0268b332fd7ee2393c78349aaaa775d9e
Ryan Sims <resims@bsu.edu>
2018-10-12 14:59:47 -0400

    Separates CLI and SQLProcessor functionality

--- src/edu/bsu/cs222/CLI.java
+++ src/edu/bsu/cs222/CLI.java
@@ -1,28 +1,20 @@
 package edu.bsu.cs222;
 
-import java.util.Scanner;
-import java.sql.*;
-//import java.sql.
-import java.sql.Connection;
-import java.sql.DriverManager;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 public class CLI {
-    static Connection conn = null;
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/library?" +
-                            "user=root&useSSL=false");
 
-            //checkout("A1334");
-            ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM books");
+            ResultSet rs = SQLProcessor.veiwAllBooks();
             String CallNumber="A1334";
-            parse(rs);
-            checkout(CallNumber);
-            rs = conn.createStatement().executeQuery("SELECT * FROM books");
-            parse(rs);
+            SQLProcessor.parse(rs);
+            SQLProcessor.checkout("A1334");
+            rs = SQLProcessor.veiwAllBooks();
+            SQLProcessor.parse(rs);
 
 
         } catch (SQLException ex) {
@@ -32,48 +24,4 @@ public class CLI {
             System.out.println("VendorError: " + ex.getErrorCode());
         }
     }
-
-    public static void parse(ResultSet resultSet) throws SQLException {
-        ResultSetMetaData metaData = resultSet.getMetaData();
-        for (int i = 1; i <= metaData.getColumnCount(); i++) {
-            System.out.print(metaData.getColumnName(i) + ", ");
-        }
-        System.out.println();
-        while (resultSet.next()) {
-            for (int i = 1; i <= metaData.getColumnCount(); i++) {
-                if (i > 1) System.out.print(",  ");
-                String columnValue = resultSet.getString(i);
-                System.out.print(columnValue);
-            }
-            System.out.println();
-        }
-    }
-    public static int getAvailable(ResultSet resultSet) throws SQLException {
-        int number=0;
-
-        if (resultSet.first()) {
-            number=resultSet.getInt(1);
-        }
-        return number;
-
-    }
-    public static void checkout(String CallNumber) {
-        int NumberAvailable = 0;
-        try {
-            ResultSet rs =conn.createStatement().executeQuery("select `Number Available` from books where `Call Number`=\"" + CallNumber + "\";");
-            NumberAvailable=getAvailable(rs);
-            if (NumberAvailable != 0) {
-                conn.createStatement().execute("update books set `Number Available`=`Number Available`-1 where `Call Number`=\"" + CallNumber + "\";");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
-
-
-
-
-
-
--- test/tests.java
+++ test/tests.java
@@ -11,6 +11,7 @@ public class tests {
         Assert.assertEquals("Book1 ISBN",book1.ISBN);
         Assert.assertEquals("Book1 Call#",book1.callnumber);
     }
+    @Test
     public void GetBook(){
         Book book1=new Book("Book1 Title", "Book1 Author", "Book1 ISBN", "Book1 Call#");
         Assert.assertEquals("Book1 Title",book1.title);
