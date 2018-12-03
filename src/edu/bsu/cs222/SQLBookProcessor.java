b951ec78a568f2de7b902f3355fc5808bac2da12
Adam Null <42658093+anull123@users.noreply.github.com>
2018-12-03 14:12:30 -0500

    Add damage report class

---
+++ src/edu/bsu/cs222/SQLBookProcessor.java
@@ -0,0 +1,73 @@
+package edu.bsu.cs222;
+
+
+import java.sql.*;
+import java.sql.SQLException;
+
+
+class SQLBookProcessor{
+
+
+    static ResultSet viewAllBooks() {
+        return SQLProcessor.generateQueryResultSet(SQLGenerator.viewAllBooks());
+    }
+    static ResultSet viewAllTransactions() {
+        return SQLProcessor.generateQueryResultSet("Select* from circulation;");
+    }
+
+    static ResultSet search(String type, String term) {
+        return advancedSearch(type,term,0);
+    }
+    static ResultSet advancedSearch(String type, String term, int qualifier) {
+        //qualifier is -1 for %term (ends with), 0 for %term% (contains), 1 for term% (starts with)
+        return SQLProcessor.generateQueryResultSet(SQLGenerator.search(type,term,qualifier));
+    }
+
+    private static int getAvailable(int barcode_number) {
+        int number = 0;
+        ResultSet rs=SQLProcessor.generateQueryResultSet(SQLGenerator.getAvailable(barcode_number));
+        try {
+            if (rs.first()) {
+                number = rs.getInt(1);
+            }
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+        return number;
+    }
+    static boolean checkout(int barcode_number, int UserID){
+        int NumberAvailable = getAvailable(barcode_number);
+        if (NumberAvailable != 0) {
+            return true;
+        //SQLProcessor.executeSQL(SQLGenerator.checkout(barcode_number,UserID));
+        }else{return false;}
+    }
+
+
+    static boolean checkin(int barcode_number){
+        int NumberAvailable = getAvailable(barcode_number);
+        if (NumberAvailable !=1 ) {
+            return true;
+            //SQLProcessor.executeSQL(SQLGenerator.checkin(barcode_number));
+        }else{return false;}
+    }
+
+    private static String getDamage(int barcode_number){
+        String damageNotes;
+        damageNotes = SQLGenerator.getDamage(barcode_number);
+        if (damageNotes == null) {
+            return "This is no known damage for this item";
+        }
+        else return damageNotes;
+    }
+
+    private static String editDamage(int barcode_number, String damageNotes) {
+        return SQLGenerator.editDamage(barcode_number, damageNotes);
+    }
+}
+
+
+
+
+
+
