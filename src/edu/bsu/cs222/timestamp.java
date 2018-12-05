4c10fc879d6804e61bc66ac171e5fc46f60ddba9
Adam Null <42658093+anull123@users.noreply.github.com>
2018-12-05 14:33:47 -0500

    Add ability to get current time and date

---
+++ src/edu/bsu/cs222/timestamp.java
@@ -0,0 +1,14 @@
+package edu.bsu.cs222;
+
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class timestamp {
+    public static void main(String[] args) {
+        //This provides a string containing the current time and date
+        DateFormat currentDateAndTime = new SimpleDateFormat("hh:mm aa dd/MM/yyyy");
+        String currentDateAndTimeString  = currentDateAndTime.format(new Date());
+        System.out.println(currentDateAndTimeString);
+    }
+}
