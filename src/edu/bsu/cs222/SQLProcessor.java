33c91b84670d55ff39912c59c9031f50b1fceef0
Adam Null <42658093+anull123@users.noreply.github.com>
2018-12-06 21:04:42 -0500

    Add Damage Report function

---
+++ src/edu/bsu/cs222/SQLProcessor.java
@@ -0,0 +1,110 @@
+package edu.bsu.cs222;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class SQLProcessor {
+    private static Connection conn;
+
+    static {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/library?user=root&useSSL=false&allowMultiQueries=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=EST");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static ResultSet generateQueryResultSet(String statement) {
+        ResultSet rs=null;
+        try {
+            rs = conn.createStatement().executeQuery(statement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    static boolean executeSQL(String statement) {
+        try {
+            conn.createStatement().execute(statement);
+            return true;
+        } catch(SQLIntegrityConstraintViolationException e){
+            System.out.println("cannot add user");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    static void parse(ResultSet resultSet) {
+        ResultSetMetaData metaData;
+        try {
+            metaData = resultSet.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                System.out.print(metaData.getColumnName(i) + ", ");
+            }
+            System.out.println();
+            while (resultSet.next()) {
+                for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = resultSet.getString(i);
+                    System.out.print(columnValue);
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    static String parseasString(ResultSet resultSet){
+        ResultSetMetaData metaData;
+        StringBuilder result= new StringBuilder();
+        try {
+            metaData = resultSet.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                result.append(metaData.getColumnName(i)).append(", ");
+            }
+            result.append("\n");
+            while (resultSet.next()) {
+                for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                    if (i > 1) result.append(",  ");
+                    String columnValue = resultSet.getString(i);
+                    result.append(columnValue);
+                }
+                result.append("\n");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.print(result.toString());
+        return result.toString();
+    }
+    static ArrayList parseasList(ResultSet resultSet) {
+        ResultSetMetaData metaData;
+        ArrayList<List<String>> list=new ArrayList<>();
+        try {
+            metaData = resultSet.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                System.out.print(metaData.getColumnName(i) + ", ");
+            }
+            System.out.println();
+            ArrayList<String> innerlist=new ArrayList<>();
+            while (resultSet.next()) {
+                innerlist.clear();
+                for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = resultSet.getString(i);
+                    System.out.print(columnValue);
+                    innerlist.add(columnValue);
+                }
+                list.add(innerlist);
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
---
+++ src/edu/bsu/cs222/SQLUserProcessor.java
@@ -0,0 +1,40 @@
+package edu.bsu.cs222;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+class SQLUserProcessor {
+    static String getUserType(String username){
+        ResultSet rs = SQLProcessor.generateQueryResultSet(SQLGenerator.getUserType(username));
+        try {
+            if (rs.first()) {
+                return rs.getString(1);
+            }
+
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+        return "Guest";
+    }
+
+    private static boolean isValid(String username){
+        return !username.contains("\"") && !username.contains("%") && !username.contains(";") && !username.contains("'");
+    }
+    //I got rid of the addUser(username, password), and instead, it will be taken care of at the interface level, if role is blank, change it to patron.
+    //-Ryan
+
+    static boolean addUser(String username, String password, String type){
+    //hash the password so that each character is encoded into index*ASCII +next+next etc
+        if (isValid(username)){
+            return SQLProcessor.executeSQL(SQLGenerator.addUser(username, password, type));
+            //returns true if added successfully, false if it didn't add
+            //the username column is set to unique in the SQL database, so that takes care of vetting duplicate usernames
+        }
+        return false; //Failed to add username due to invalid characters
+    }
+    static boolean login(String username, String password){
+        return SQLProcessor.parseasString(SQLProcessor.generateQueryResultSet(SQLGenerator.getPassword(username))).contains(password);
+        //if the username does not exist, the results should be empty.
+        //if the username does exist, the sql query responds with the password, and .contains() checks if the password matches.
+    }
+}
